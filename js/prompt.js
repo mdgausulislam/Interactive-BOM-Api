7992df5e4f3f75935eda2139457d63dbf0b6f970
unknown <gausulislam5@gmail.com>
2023-09-28 21:29:31 +0600

    update bom

---
+++ js/prompt.js
@@ -0,0 +1,22 @@
+alert('how are you?');
+const showAlert=()=>{
+    alert('how are you1?')
+}
+const showAlert2=()=>{
+  const result= confirm('how are you2?');
+  console.log(result);
+  if(result==true){
+    alert('hhhh')
+  }else{
+    console.log('vkijhvj');
+  }
+}
+const showAlert3=()=>{
+   const result1= prompt('Md Gousul Islam Hemonto');
+   console.log(result1);
+   if(result1==null){
+    alert('hhrhhgggg');
+   }else{
+    console.log('hemonto');
+   }
+}
